2839c4555a634530a348366e9174c69a2ea746ce
rajalingeshr17 <rajalingeshr00@gmail.com>
2026-01-05 00:12:24 +0530

    Update Login.jsx

--- game-front/src/components/Login.jsx
+++ game-front/src/components/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const API = "http://localhost:5000";
+const API = "https://dell-game-backend.onrender.com";
 
 export default function Login() {
   const [name, setName] = useState("");
